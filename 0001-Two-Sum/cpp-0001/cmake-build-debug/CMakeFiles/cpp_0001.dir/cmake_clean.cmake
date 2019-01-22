da320afd7d1b0c5ebd326aaf8ab3962dec12e272
刘华 <liuhua@uni-ubi.com>
2019-01-22 14:43:11 +0800

    test

---
+++ 0001-Two-Sum/cpp-0001/cmake-build-debug/CMakeFiles/cpp_0001.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cpp_0001.dir/main2.cpp.obj"
+  "cpp_0001.pdb"
+  "cpp_0001.exe"
+  "cpp_0001.exe.manifest"
+  "libcpp_0001.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/cpp_0001.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
